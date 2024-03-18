e1bd0230d830229803748d66435529704bfbc3c6
Spartan - Huy Le <144250684+spartan-huyle@users.noreply.github.com>
2024-03-18 13:36:08 +0700

    hotfix: add default subdomain (#135)

--- src/utils/domain.utils.ts
+++ src/utils/domain.utils.ts
@@ -1,15 +1,26 @@
 /**
  * Get subdomain e.g. https://intrust-bank.tryforesight.link/, https://intrust-bank.portal.tryforesight.link/
+ *                    http://intrust-bank.localhost:5173/login
  * @returns e.g. intrust-bank
  */
+
+const DEFAULT_DEMO_SUBDOMAIN = "capsight"
 
 function getSubdomain(): string {
   try {
     const { host } = window.location
+    const isDev = host.includes("localhost")
     const splitHost = host.split(".")
-    return splitHost[0]
+    const isContainPortal = splitHost.includes("portal")
+    if (
+      (!isDev && splitHost.length === (isContainPortal ? 4 : 3)) ||
+      (isDev && splitHost.length === (isContainPortal ? 3 : 2))
+    ) {
+      return splitHost[0]
+    }
+    return DEFAULT_DEMO_SUBDOMAIN
   } catch {
-    return ""
+    return DEFAULT_DEMO_SUBDOMAIN
   }
 }
 
