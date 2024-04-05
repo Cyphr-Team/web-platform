173fb6db7c550a965512a3408bb71e3ff2296fd9
Spartan - Huy Le <144250684+spartan-huyle@users.noreply.github.com>
2024-04-05 11:22:38 +0700

    fix: authenticated should not be able to access login page again (#171)

--- src/shared/layouts/auth-layout/auth-layout.tsx
+++ src/shared/layouts/auth-layout/auth-layout.tsx
@@ -1,6 +1,31 @@
-import { Outlet } from "react-router-dom"
+import { APP_PATH } from "@/constants"
+import { inMemoryJWTService } from "@/services/jwt.service"
+import { Loader2 } from "lucide-react"
+import { useState } from "react"
+import { Outlet, useNavigate } from "react-router-dom"
+import { useMount } from "react-use"
 
 export function AuthLayout() {
+  const [checkLoggedIn, setCheckLoggedIn] = useState(true)
+  const navigate = useNavigate()
+
+  // check if user is logged in, navigate to homepage
+  useMount(() => {
+    inMemoryJWTService
+      .getNewAccessToken()
+      .then(() => {
+        navigate(APP_PATH.INDEX, { replace: true })
+      })
+      .catch(() => {
+        // Do nothing
+      })
+      .finally(() => {
+        setCheckLoggedIn(false)
+      })
+  })
+
+  if (checkLoggedIn) return <Loader2 className="animate-spin" />
+
   return (
     <div>
       <Outlet />
