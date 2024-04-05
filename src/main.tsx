659804736963fbae00ef2f974e3c4ca4101b83e6
Spartan - Huy Le <144250684+spartan-huyle@users.noreply.github.com>
2024-04-05 11:21:05 +0700

    fix: error loading previous deployed file (#170)

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import App from "./App.tsx"
 import "./index.css"
 import "react-phone-number-input/style.css"
 
+window.addEventListener("vite:preloadError", () => {
+  // Refresh the page when load lazy import component
+  // https://vitejs.dev/guide/build.html#load-error-handling
+  window.location.reload()
+})
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
