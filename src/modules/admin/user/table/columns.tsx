10446a875183ecc917fc8379b7dcf6fe2f6a55fa
Ngan Phan <143688130+spartan-nganphan@users.noreply.github.com>
2024-04-24 18:54:30 +0700

    MVP-1162: modify members tracking table (#220)

--- src/modules/admin/user/table/columns.tsx
+++ src/modules/admin/user/table/columns.tsx
@@ -1,16 +1,24 @@
 import { Badge } from "@/components/ui/badge"
 import { DataTableColumnHeader } from "@/shared/molecules/table/column-header"
 import { UserDetailInfo } from "@/types/user.type"
-import { snakeCaseToText } from "@/utils"
-import { formatDate } from "@/utils/date.utils"
+import { convertToReadableDateAgo, snakeCaseToText } from "@/utils"
 import { ColumnDef } from "@tanstack/react-table"
 
 export const columns: ColumnDef<UserDetailInfo>[] = [
   {
-    accessorKey: "email",
+    accessorKey: "name",
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Email" />
-    )
+      <DataTableColumnHeader column={column} title="Name" />
+    ),
+    cell: ({ row }) => {
+      const application = row.original
+      return (
+        <>
+          <div className="text-black font-medium">{application.name}</div>
+          <div className=" text-gray-400">{application.email}</div>
+        </>
+      )
+    }
   },
   {
     accessorKey: "authProvider",
@@ -34,17 +42,13 @@ export const columns: ColumnDef<UserDetailInfo>[] = [
     }
   },
   {
-    accessorKey: "createdAt",
-    header: () => <p>Created on</p>,
+    accessorKey: "lastActive",
+    header: () => <p>Last Active</p>,
     size: 150,
     cell: ({ row }) => {
       const application = row.original
 
-      return (
-        <div>
-          {application.createdAt ? formatDate(application.createdAt) : "N/A"}
-        </div>
-      )
+      return <div>{convertToReadableDateAgo(application.loggedInAt)}</div>
     }
   }
 ]
