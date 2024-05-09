90c864bd163179fbd44d89ed32809d094d0e2e0e
Quang Nguyen <118340420+spartan-quangnguyen@users.noreply.github.com>
2024-05-09 11:58:33 +0700

    Update default public endpoint (#243)

--- src/lib/aws.ts
+++ src/lib/aws.ts
@@ -2,6 +2,6 @@ import { APP_CONFIGS } from "@/configs"
 
 const awsConfig = {
   publicAssetDomain:
-    APP_CONFIGS.VITE_PUBLIC_ENDPOINT ?? "https://cdn.tryforesight.link"
+    APP_CONFIGS.VITE_PUBLIC_ENDPOINT ?? "https://cdn.cyphrai.dev"
 }
 export const publicAssetDomain = awsConfig.publicAssetDomain
