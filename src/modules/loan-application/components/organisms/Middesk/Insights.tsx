df7c14e4228642b3ddfb81b9bd77e4beac5cb297
spartan-nganphan <ngan.phan@c0x12c.com>
2024-07-18 18:18:34 +0700

    change insight verification number

--- src/modules/loan-application/components/organisms/Middesk/Insights.tsx
+++ src/modules/loan-application/components/organisms/Middesk/Insights.tsx
@@ -12,7 +12,7 @@ export const Insights = () => {
   const { loanKybDetail, isLoading } = useLoanApplicationDetailContext()
 
   const insights = loanKybDetail?.insights
-
+  console.log(insights)
   const numberOfSuccess = useMemo(
     () =>
       insights
@@ -25,6 +25,7 @@ export const Insights = () => {
         : 0,
     [insights]
   )
+  const insightsTotal = insights ? Object.entries(insights).length : 0
 
   return (
     <Card className="h-fit lg:sticky top-0 z-10 mb-4 flex-shrink-0">
@@ -34,7 +35,9 @@ export const Insights = () => {
           {isLoading ? (
             <Skeleton className="w-16 h-4" />
           ) : (
-            <div>{numberOfSuccess}/7</div>
+            <div>
+              {numberOfSuccess}/{insightsTotal}
+            </div>
           )}
         </CardTitle>
         <Separator />
