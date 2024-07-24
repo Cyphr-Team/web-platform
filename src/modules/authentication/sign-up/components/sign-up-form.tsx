820bb618d331e7b04c05603cbfdd0427fae8fced
spartan-sangkha <sang.kha@c0x12c.com>
2024-07-24 21:39:13 +0700

    chore: add FF for terms agreement

--- src/modules/authentication/sign-up/components/sign-up-form.tsx
+++ src/modules/authentication/sign-up/components/sign-up-form.tsx
@@ -21,6 +21,7 @@ import { useState } from "react"
 import { AlertTriangle } from "lucide-react"
 import { Checkbox } from "@/components/ui/checkbox"
 import { CheckedState } from "@radix-ui/react-checkbox"
+import { isEnableTermAgreementsCheckbox } from "@/utils/feature-flag.utils"
 
 export function SignUpForm() {
   const { isPending, mutate } = useGetStart()
@@ -36,19 +37,25 @@ export function SignUpForm() {
   })
 
   const formSubmit = form.handleSubmit((data) => {
-    if (!agreedToTerms) {
-      setShowError(true)
+    if (isEnableTermAgreementsCheckbox()) {
+      if (!agreedToTerms) {
+        setShowError(true)
+      } else {
+        setShowError(false)
+        mutate(data)
+      }
     } else {
-      setShowError(false)
       mutate(data)
     }
   })
 
   const handleCheckboxChange = (checked: CheckedState) => {
-    const value = checked.valueOf() as boolean
-    setAgreedToTerms(value)
-    if (value) {
-      setShowError(false)
+    if (isEnableTermAgreementsCheckbox()) {
+      const value = checked.valueOf() as boolean
+      setAgreedToTerms(value)
+      if (value) {
+        setShowError(false)
+      }
     }
   }
 
@@ -78,49 +85,51 @@ export function SignUpForm() {
             )}
           />
 
-          <FormItem className="space-y-4">
-            <div className="flex items-center space-x-2">
-              <Checkbox
-                id="terms"
-                checked={agreedToTerms}
-                onCheckedChange={handleCheckboxChange}
-                className="h-4 w-4"
-              />
-              <FormLabel
-                htmlFor="terms"
-                className="text-xs text-muted-foreground"
-              >
-                I agree to the{" "}
-                <a
-                  href="https://www.cyphrai.com/terms"
-                  className="underline"
-                  rel="noopener noreferrer"
-                  target="terms"
+          {isEnableTermAgreementsCheckbox() && (
+            <FormItem className="space-y-4">
+              <div className="flex items-center space-x-2">
+                <Checkbox
+                  id="terms"
+                  checked={agreedToTerms}
+                  onCheckedChange={handleCheckboxChange}
+                  className="h-4 w-4"
+                />
+                <FormLabel
+                  htmlFor="terms"
+                  className="text-xs text-muted-foreground"
                 >
-                  Terms of Service
-                </a>{" "}
-                and{" "}
-                <a
-                  href="https://www.cyphrai.com/privacy"
-                  className="underline"
-                  rel="noopener noreferrer"
-                  target="privacy"
-                >
-                  Privacy Policy
-                </a>
-                .
-              </FormLabel>
-            </div>
-            {showError && (
-              <FormMessage className="flex text-base items-center font-light rounded-md p-2 border-2 border-[#FB9999] bg-[#FEE6E6]">
-                <AlertTriangle size={32} className="text-[#C40000] mr-2" />
-                <span className="text-xs text-[#B42318] font-medium">
-                  You must accept the Terms of Service and Privacy Policy to
-                  create an account
-                </span>
-              </FormMessage>
-            )}
-          </FormItem>
+                  I agree to the{" "}
+                  <a
+                    href="https://www.cyphrai.com/terms"
+                    className="underline"
+                    rel="noopener noreferrer"
+                    target="terms"
+                  >
+                    Terms of Service
+                  </a>{" "}
+                  and{" "}
+                  <a
+                    href="https://www.cyphrai.com/privacy"
+                    className="underline"
+                    rel="noopener noreferrer"
+                    target="privacy"
+                  >
+                    Privacy Policy
+                  </a>
+                  .
+                </FormLabel>
+              </div>
+              {showError && (
+                <FormMessage className="flex text-base items-center font-light rounded-md p-2 border-2 border-[#FB9999] bg-[#FEE6E6]">
+                  <AlertTriangle size={32} className="text-[#C40000] mr-2" />
+                  <span className="text-xs text-[#B42318] font-medium">
+                    You must accept the Terms of Service and Privacy Policy to
+                    create an account
+                  </span>
+                </FormMessage>
+              )}
+            </FormItem>
+          )}
 
           <ButtonLoading
             isLoading={isPending}
