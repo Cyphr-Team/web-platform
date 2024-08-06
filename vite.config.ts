25fcdc4adc666649463e78bdad87d7a17dd6d8a4
Huy Le <144250684+spartan-huyle@users.noreply.github.com>
2024-08-06 11:19:03 +0700

    Fix developement plugin (#520)

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import path from "path"
-import { defineConfig } from "vite"
-
+import react from "@vitejs/plugin-react"
 import { createRequire } from "node:module"
-
-import { normalizePath } from "vite"
+import path from "path"
+import { defineConfig, normalizePath } from "vite"
 import { viteStaticCopy } from "vite-plugin-static-copy"
 
+/**
+ * Support for non-latin characters
+ * https://github.com/wojtekmaj/react-pdf?tab=readme-ov-file#support-for-non-latin-characters
+ */
 const require = createRequire(import.meta.url)
 const cMapsDir = normalizePath(
   path.join(path.dirname(require.resolve("pdfjs-dist/package.json")), "cmaps")
@@ -19,6 +21,7 @@ const standardFontsDir = normalizePath(
 
 export default defineConfig({
   plugins: [
+    react(),
     viteStaticCopy({
       targets: [
         { src: cMapsDir, dest: "" },
