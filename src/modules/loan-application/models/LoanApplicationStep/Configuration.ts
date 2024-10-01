e3406ca8f87f4adbde9c07a6361d72a1df6a5cf0
vutnguyen <vu.tran.nguyen@c0x12c.com>
2024-10-01 23:15:52 +0700

    refactor: add missing form configuration

--- src/modules/loan-application/models/LoanApplicationStep/Configuration.ts
+++ src/modules/loan-application/models/LoanApplicationStep/Configuration.ts
@@ -11,11 +11,13 @@ export class ConfigurationLoanApplicationStep
   }
 
   _buildSteps(formTypes: FORM_TYPE[] = []) {
-    // Currently, we MUST HAVE LoanRequestStep and ConfirmationStep
+    // Currently, we MUST HAVE ConfirmationStep
     // We will separate them later
-    this._build_LoanRequestStep()
     formTypes.forEach((formType) => {
       switch (formType) {
+        case FORM_TYPE.LOAN_REQUEST:
+          this._build_LoanRequestStep()
+          break
         case FORM_TYPE.KYB:
           this._build_BusinessInformationStep()
           break
@@ -36,6 +38,39 @@ export class ConfigurationLoanApplicationStep
           break
         case FORM_TYPE.IDENTITY_VERIFICATION:
           this._build_IdentityVerificationStep()
+          break
+        case FORM_TYPE.PRODUCT_SERVICE:
+          this._build_ProductServiceStep()
+          break
+        case FORM_TYPE.MARKET_OPPORTUNITY:
+          this._build_MarketOpportunityStep()
+          break
+        case FORM_TYPE.BUSINESS_MODEL:
+          this._build_BusinessModelStep()
+          break
+        case FORM_TYPE.EXECUTION:
+          this._build_ExecutionStep()
+          break
+        case FORM_TYPE.LAUNCH_KC_FIT:
+          this._build_LaunchKCFitStep()
+          break
+        case FORM_TYPE.PRE_QUALIFICATION:
+          this._build_PreQualificationStep()
+          break
+        case FORM_TYPE.BUSINESS_EIN_LETTER:
+          this._build_BusinessEINLetterStep()
+          break
+        case FORM_TYPE.CERTIFICATE_OF_GOOD_STANDING:
+          this._build_CertificateOfGoodStanding()
+          break
+        case FORM_TYPE.FICTITIOUS_NAME_CERTIFICATION:
+          this._build_FictitiousNameCertification()
+          break
+        case FORM_TYPE.ARTICLES_OF_ORGANIZATION:
+          this._build_ArticlesOfOrganization()
+          break
+        case FORM_TYPE.BY_LAWS:
+          this._build_ByLaws()
           break
         case FORM_TYPE.REVIEW_APPLICATION:
           this._build_ReviewApplicationStep()
