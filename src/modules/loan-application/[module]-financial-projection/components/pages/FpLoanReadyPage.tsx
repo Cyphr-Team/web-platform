9e90318cf8f1cfce0af55b3eba712b7d8e0c130b
Huy Le <144250684+spartan-huyle@users.noreply.github.com>
2024-10-09 11:50:05 +0700

    FP - Loan Ready - Integrate API (#774)

--- src/modules/loan-application/[module]-financial-projection/components/pages/FpLoanReadyPage.tsx
+++ src/modules/loan-application/[module]-financial-projection/components/pages/FpLoanReadyPage.tsx
@@ -1,13 +1,17 @@
-import { useQueryLoanReadinessAssessmentByApplicationId } from "@/modules/conference-demo/admin/hooks/conference/useQuery"
+import { useQueryLoanReadinessAssessmentByApplicationId } from "@/modules/loan-application-management/hooks/loan-readiness-assessment/useQueryLoanReadinessAssessment"
 import { FinancialApplicationAssessment } from "@/modules/loan-application-management/pages/cyphr-flex/FinancialApplicationAssessment"
 import { FinancialApplicationCriteria } from "@/modules/loan-application-management/pages/cyphr-flex/FinancialApplicationCriteria"
+import { useParams } from "react-router-dom"
 
 export function Component() {
   return <LoanReadiness />
 }
 
 export const LoanReadiness = () => {
-  const { data, isLoading } = useQueryLoanReadinessAssessmentByApplicationId()
+  const { id } = useParams()
+  const { data, isLoading } = useQueryLoanReadinessAssessmentByApplicationId({
+    applicationId: id
+  })
 
   return (
     <div className="flex flex-col gap-8">
